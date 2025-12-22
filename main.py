1e9306f1e76faa40d64ef1a9d15c665cb88a1426
Ray Bell <ray.bell@maryland.gov>
2025-12-22 16:52:17 -0500

    init main

---
+++ main.py
@@ -0,0 +1,10 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "google-genai>=1.56.0",
+#     "google-api-python-client>=2.187.0",
+#     "isodate>=0.7.2",
+#     "youtube-transcript-api>=1.2.3"
+# ]
+# ///
+print("hello world")
